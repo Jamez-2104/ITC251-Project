3112c1cb795a79afda03a26f7feb7d534c41bc91
mgonzalez907 <mgonzalez907>
2026-04-12 19:01:11 -0700

    added a way for program to detect if user is asking for a txt file && added where pattrn first appears in text provided

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Usage: ./grep-lite [flags] pattern\n");
+        printf("Usage: ./grep-lite [flags] [pattern][text]\n");
         return 1;
     }
 
@@ -11,6 +12,10 @@ int main(int argc, char *argv[]) {
     int w_flag = 0;
     int c_flag = 0;
     char *pattern = NULL;
+    char *text = NULL;
+    char *patternText = NULL;
+    char *userFile = NULL;
+    char *line = NULL;
 
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] == '-') {
@@ -23,13 +28,31 @@ int main(int argc, char *argv[]) {
             } else if (argv[i][1] == 'c') {
                 c_flag = 1;
             }
-        } else {
-            pattern = argv[i];
+        pattern = argv[2];//hardcoded
+        text = argv[3];//hardcoded
         }
     }
 
+    if (strstr(text, ".txt")){//I spy with my little eye a text file
+        FILE *fptr;
+        userFile = text;
+        char line[256];
+        fptr = fopen(userFile, "r");//I will open the file
+        if(fptr == NULL){
+                printf("Error opening file.\n");//I couldnt find anyhting
+        }
+        while (fgets(line, sizeof(line), fptr)){
+                if (strstr(line, pattern)){//breaks at first sight of pattern
+                        patternText=line;//TODO add method to traverse through more sightings of pattern
+                        break;
+                }
+        }
+     }
+
     printf("Pattern: %s\n", pattern);
+    printf("Pattern in text: %s\n", patternText);
     printf("Flags: n=%d, i=%d, w=%d, c=%d\n", n_flag, i_flag, w_flag, c_flag);
 
     return 0;
 }
+
